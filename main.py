c2cb80052dd1f12c9df1bed23bd1daac2454b3d1
308 <308@academicos.cesde.edu.co>
2022-08-11 08:29:39 -0500

    NTJ2-Se-crea-main-con-datos-primitivos-y-entradas-por-teclado

---
+++ main.py
@@ -0,0 +1,24 @@
+
+# 1.Datos primitivos
+nombre = 'Daniela'
+edad = 26
+estatura = 1.64
+hinchaDelMejor = True
+print(f'Hola {nombre} ¿cómo estás?, tienes {edad} años, mide {estatura}cm')
+
+# 2. Datos de entrada por teclado
+'''apellido = input('Digite su apellido: ')
+print(f'Su apellido es {apellido}')'''
+'''numero1 = int(input('Digite un numero: '))
+numero2 = int(input('Digite un numero: '))
+suma = numero1 + numero2
+print(f'El resultado es: {suma}')'''
+
+#Retos 
+multiplo = int(input('Ingrese un numero'))
+numero = 5
+resultado = int(multiplo / 5)
+if (resultado % 0):
+    print('Si es multiplo')
+else: 
+    print('No es')
